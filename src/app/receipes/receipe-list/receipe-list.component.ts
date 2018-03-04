a357859bc24a4ef10e4ed4847fef1c2569985509
Adarsh <adarshlr76@gmail.com>
2018-03-04 12:54:53 +0530

    first commit

---
+++ src/app/receipes/receipe-list/receipe-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {Receipe} from '../receipe.model';
+
+@Component({
+  selector: 'app-receipe-list',
+  templateUrl: './receipe-list.component.html',
+  styleUrls: ['./receipe-list.component.css']
+})
+export class ReceipeListComponent implements OnInit {
+
+  //receipes = [];
+  receipes: Receipe[] = [
+    new Receipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Receipe('A Test1 Recipe', 'This is also simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  ];
+
+  constructor() { }
+
+
+
+  ngOnInit() {
+  }
+
+}
